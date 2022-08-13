8bd1efa3c200c981744fd5b6e50fe3fef905f056
parsa <hi@parsam.io>
2022-08-13 15:33:05 +0100

    move folders outside of src

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import { NextPageWithLayout } from "./_app";
+
+const IndexPage: NextPageWithLayout = () => {
+  return (
+    <>
+      <h1>Booksplore</h1>
+      <p>Explore your next read.</p>
+
+      <h2>Books</h2>
+    </>
+  );
+};
+
+export default IndexPage;
