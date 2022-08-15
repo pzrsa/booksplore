1524c62605f3e7308c0ccecbe492f9e4e9c60a35
parsa <hi@parsam.io>
2022-08-15 21:30:54 +0100

    display books from database

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,55 @@
-import type { GetStaticProps } from "next/types";
-import { NextPageWithLayout } from "./_app";
+import { Prisma } from "@prisma/client";
+import Image from "next/image";
+import type { InferGetStaticPropsType } from "next/types";
+import { populateDatabase } from "utils/notion";
+import { prisma } from "../utils/prisma";
 
-const IndexPage: NextPageWithLayout = () => {
+const defaultBookSelect = Prisma.validator<Prisma.BookSelect>()({
+  id: true,
+  title: true,
+  authorId: true,
+  genreId: true,
+  asin: true,
+  isbn13: true,
+  createdAt: false,
+  updatedAt: false,
+});
+
+const IndexPage = ({
+  books,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <>
       <h1>Booksplore</h1>
       <p>Explore your next read.</p>
 
       <h2>Books</h2>
+      {books.map((book) => (
+        <div key={book.id}>
+          <Image
+            src={`https://images-eu.ssl-images-amazon.com/images/P/${book.asin}._LZZZZZZZ_.jpg`}
+            width={400}
+            height={600}
+            alt={`${book.title} Cover`}
+          />
+          <p>{book.title}</p>
+        </div>
+      ))}
     </>
   );
 };
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getStaticProps = async () => {
+  try {
+    await populateDatabase();
+  } catch (err) {
+    console.error(err);
+  }
+
+  const books = await prisma.book.findMany({ select: defaultBookSelect });
+
   return {
-    props: {},
+    props: { books },
   };
 };
 
