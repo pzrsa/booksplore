fa316c59c71e5c6e9ee298f7243a492c073ce574
parsa <hi@parsam.io>
2022-08-21 22:53:06 +0100

    update prisma schema to remove status from save

---
+++ prisma/migrations/20220821205846_remove_status/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `status` on the `saves` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "saves" DROP COLUMN "status";
