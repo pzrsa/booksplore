999de3896828e20799ce9c01b92a4a75ff545d43
parsa <hi@parsam.io>
2022-08-21 12:48:42 +0100

    remove constants

--- utils/constants.ts
+++
@@ -1,5 +0,0 @@
-export const NOTION_TITLE_ID = "title";
-export const NOTION_AUTHOR_ID = "Y%5BoK";
-export const NOTION_GENRE_ID = "pTI%3D";
-export const NOTION_ISBN13_ID = "HhDx";
-export const NOTION_ASIN_ID = "YEfU";
