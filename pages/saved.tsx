4680ff6770a5656e0fdaf5f68453874089a76c9e
parsa <hi@parsam.io>
2022-08-22 18:43:23 +0100

    create add to amazon cart link

--- pages/saved.tsx
+++ pages/saved.tsx
@@ -12,30 +12,46 @@ import { authOptions } from "./api/auth/[...nextauth]";
 const Saved = ({
   user,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  // https://www.amazon.co.uk/gp/aws/cart/add.html?ASIN.1=1988575117&ASIN.2=0241453577 is valid
+  let url = "https://www.amazon.co.uk/gp/aws/cart/add.html?";
+  user?.saves.forEach((save, index) => {
+    url = url.concat(`ASIN.${index + 1}=${save.book.asin}&`);
+  });
+
   return (
     <>
+      <h1 className={"text-3xl mb-2"}>
+        <a
+          href={url}
+          rel="prefetch noreferrer"
+          target="_blank"
+          className="hover:text-neutral-500 dark:group-hover:text-neutral-400 transition-all text-3xl sm:text-4xl font-bold"
+        >
+          add to amazon cart
+        </a>
+      </h1>
       <div className="grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7">
         {user?.saves.map((save) => (
           <div key={save.book.id}>
-            <Link href={`/b/${save.book.isbn13}`}>
-              <a>
-                <div className="aspect-w-4 aspect-h-6">
+            <div className="aspect-w-4 aspect-h-6">
+              <Link href={`/b/${save.book.isbn13}`}>
+                <a>
                   <Image
                     src={`https://images-eu.ssl-images-amazon.com/images/P/${save.book.asin}._LZZZZZZZ_.jpg`}
                     alt={`${save.book.title} cover`}
                     layout={"fill"}
                   />
-                </div>
-                <h1 className={"text-xl"}>
-                  {save.book.title} • {save.book.author.name}
-                </h1>
-              </a>
-            </Link>
-            <Link href={`/g/${save.book.genre}`}>
-              <a>
-                <h3 className={"text-lg capitalize"}>{save.book.genre}</h3>
-              </a>
-            </Link>
+                </a>
+              </Link>
+            </div>
+            <h1 className={"text-xl"}>
+              {save.book.title} • {save.book.author.name}
+            </h1>
+            <h3 className={"text-lg capitalize"}>
+              <Link href={`/g/${save.book.genre}`}>
+                <a>{save.book.genre}</a>
+              </Link>
+            </h3>
           </div>
         ))}
       </div>
