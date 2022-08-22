65c1dbfcc01d6d248455713908d55f8232bb1597
parsa <hi@parsam.io>
2022-08-22 21:27:07 +0100

    add bookcard to other pages

--- pages/g/[id].tsx
+++ pages/g/[id].tsx
@@ -1,6 +1,5 @@
+import BookCard from "components/BookCard";
 import { prisma } from "lib/prisma";
-import Image from "next/image";
-import Link from "next/link";
 import type {
   GetStaticPaths,
   GetStaticPropsContext,
@@ -11,24 +10,9 @@ import { defaultBookSelect } from "../../lib/types";
 const Genre = ({ books }: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <>
-      <div className="grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7">
+      <div className="gap-y-10 xl:gap-x-6 gap-x-4 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7">
         {books.map((book) => (
-          <div key={book.id}>
-            <div className="aspect-w-4 aspect-h-6">
-              <Link href={`/b/${book.isbn13}`}>
-                <a>
-                  <Image
-                    src={`https://images-eu.ssl-images-amazon.com/images/P/${book.asin}._LZZZZZZZ_.jpg`}
-                    alt={`${book.title} cover`}
-                    layout={"fill"}
-                  />
-                </a>
-              </Link>
-            </div>
-            <h1 className={"text-xl"}>
-              {book.title} • {book.author.name}
-            </h1>
-          </div>
+          <BookCard key={book.id} book={book} />
         ))}
       </div>
     </>
--- pages/saved.tsx
+++ pages/saved.tsx
@@ -1,6 +1,5 @@
+import BookCard from "components/BookCard";
 import { unstable_getServerSession } from "next-auth/next";
-import Image from "next/image";
-import Link from "next/link";
 import type {
   GetServerSidePropsContext,
   InferGetServerSidePropsType,
@@ -30,29 +29,9 @@ const Saved = ({
           add to amazon cart
         </a>
       </h1>
-      <div className="grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7">
+      <div className="gap-y-10 xl:gap-x-6 gap-x-4 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7">
         {user?.saves.map((save) => (
-          <div key={save.book.id}>
-            <div className="aspect-w-4 aspect-h-6">
-              <Link href={`/b/${save.book.isbn13}`}>
-                <a>
-                  <Image
-                    src={`https://images-eu.ssl-images-amazon.com/images/P/${save.book.asin}._LZZZZZZZ_.jpg`}
-                    alt={`${save.book.title} cover`}
-                    layout={"fill"}
-                  />
-                </a>
-              </Link>
-            </div>
-            <h1 className={"text-xl"}>
-              {save.book.title} • {save.book.author.name}
-            </h1>
-            <h3 className={"text-lg capitalize"}>
-              <Link href={`/g/${save.book.genre}`}>
-                <a>{save.book.genre}</a>
-              </Link>
-            </h3>
-          </div>
+          <BookCard key={save.book.id} book={save.book} />
         ))}
       </div>
     </>
