f7e2aeaaeef37431c05e635993d6b3d9d5a4707a
parsa <hi@parsam.io>
2022-08-27 19:57:14 +0100

    use fallback data for swr with ssr

--- pages/saved.tsx
+++ pages/saved.tsx
@@ -1,48 +1,46 @@
-import useSWR from "swr";
-import BookCard from "../components/BookCard";
-import fetcher from "../lib/fetcher";
+import { prisma } from "lib/prisma";
+import { GetServerSidePropsContext } from "next";
+import { unstable_getServerSession } from "next-auth/next";
+import SavedBooks from "../components/SavedBooks";
+import { defaultUserSelect } from "../lib/types";
+import { authOptions } from "./api/auth/[...nextauth]";
 
 const Saved = ({}) => {
-  const { data, error, isValidating } = useSWR("/api/me", fetcher);
-
-  if (error) {
-    return <p>failed to load</p>;
-  }
-
-  if (!data && isValidating) {
-    return <p>loading...</p>;
-  }
-
-  let url = "https://www.amazon.co.uk/gp/aws/cart/add.html?";
-  data.saves.forEach((save: any, index: number) => {
-    url = url.concat(`ASIN.${index + 1}=${save.book.asin}&`);
-  });
-
   return (
     <div>
-      {data.saves.length > 0 ? (
-        <div>
-          <h1 className={"text-3xl mb-2"}>
-            <a
-              href={url}
-              rel="prefetch noreferrer"
-              target="_blank"
-              className="hover:text-neutral-500 dark:group-hover:text-neutral-400 transition-all text-3xl sm:text-4xl font-bold"
-            >
-              add to amazon cart
-            </a>
-          </h1>
-          <div className="gap-y-10 xl:gap-x-6 gap-x-4 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7">
-            {data.saves.map((save: any) => (
-              <BookCard key={save.book.id} book={save.book} />
-            ))}
-          </div>
-        </div>
-      ) : (
-        <p>no books saved</p>
-      )}
+      <SavedBooks />
     </div>
   );
+};
+
+export const getServerSideProps = async ({
+  req,
+  res,
+}: GetServerSidePropsContext) => {
+  const session = await unstable_getServerSession(req, res, authOptions);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+      props: {},
+    };
+  }
+
+  const user = await prisma.user.findUnique({
+    where: { email: session.user?.email as string },
+    select: defaultUserSelect,
+  });
+
+  return {
+    props: {
+      fallback: {
+        "/api/me": user,
+      },
+    },
+  };
 };
 
 export default Saved;
