aba2f91db88bae7667ff300220d1229ea1f4badf
parsa <hi@parsam.io>
2022-09-16 23:16:51 +0100

    update packages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,32 +1,16 @@
-import { NextPage } from "next";
 import { SessionProvider } from "next-auth/react";
-import { AppProps } from "next/app";
 import { AppType } from "next/dist/shared/lib/utils";
-import { ReactElement, ReactNode } from "react";
 import { Layout } from "../components/Layout";
 import "../styles/globals.css";
 
-export type NextPageWithLayout = NextPage & {
-  getLayout?: (page: ReactElement) => ReactNode;
-};
-
-type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout;
-};
-
-const MyApp = (({
-  Component,
-  pageProps: { session, ...pageProps },
-}: AppPropsWithLayout) => {
-  const getLayout =
-    Component.getLayout ??
-    ((page) => (
-      <SessionProvider session={session}>
-        <Layout>{page}</Layout>
-      </SessionProvider>
-    ));
-
-  return getLayout(<Component {...pageProps} />);
+const MyApp = (({ Component, pageProps: { session, ...pageProps } }: any) => {
+  return (
+    <SessionProvider session={session}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
+  );
 }) as AppType;
 
 export default MyApp;
