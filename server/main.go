2547a19d10059491fac695f8b6a4bde2ddf7823b
parsa <hi@parsam.io>
2022-09-25 23:25:21 +0100

    initialise postgres database

--- server/main.go
+++ server/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"booksplore/models"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func setupRouter() *gin.Engine {
@@ -18,6 +23,19 @@ func setupRouter() *gin.Engine {
 }
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("server: error loading .env file: %s", err)
+	}
+
+	db, err := gorm.Open(postgres.Open(os.Getenv("DATABASE_URL")), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("server: failed to connect database: %s", err)
+	}
+
+	// Migrate the schema
+	db.AutoMigrate(&models.User{})
+
 	const port = ":4000"
 
 	r := setupRouter()
