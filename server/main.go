57178bafc7e6c8533e3e45f18d136ded1ed9efd1
parsa <hi@parsam.io>
2022-09-26 20:59:41 +0100

    remove models, focus on api first

--- server/main.go
+++ server/main.go
@@ -2,14 +2,10 @@ package main
 
 import (
 	"booksplore/api"
-	"booksplore/models"
 	"log"
-	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
 )
 
 func setupRouter() *gin.Engine {
@@ -19,7 +15,7 @@ func setupRouter() *gin.Engine {
 	v1 := r.Group("v1")
 	{
 		// Ping
-		api.Ping(v1)
+		api.GetPing(v1)
 	}
 
 	return r
@@ -30,13 +26,6 @@ func main() {
 	if err != nil {
 		log.Fatalf("server: error loading .env file: %s", err)
 	}
-
-	db, err := gorm.Open(postgres.Open(os.Getenv("DATABASE_URL")), &gorm.Config{})
-	if err != nil {
-		log.Fatalf("server: failed to connect database: %s", err)
-	}
-
-	db.AutoMigrate(&models.User{})
 
 	const port = ":4000"
 
