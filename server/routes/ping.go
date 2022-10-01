409df11e08f304947aaeea5aefb685c76999ac58
parsa <hi@parsam.io>
2022-10-01 12:25:27 +0100

    refactor routes

--- server/routes/ping.go
+++ server/routes/ping.go
@@ -2,8 +2,8 @@ package routes
 
 import "github.com/gin-gonic/gin"
 
-// GET /v1/ping
-func GetPing(r *gin.RouterGroup) {
+func ping(r *gin.RouterGroup) {
+	// GET /v1/ping
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "pong", "data": nil})
 	})
