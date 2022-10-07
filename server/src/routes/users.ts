923248dd14a4e468c9bf3c0013ebc7eae9b4ce91
parsa <hi@parsam.io>
2022-10-08 00:41:51 +0100

    update example response in users

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -3,7 +3,19 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", async (_, res) => {
-  return res.json({ message: "User received", data: { name: "parsa" } });
+  res.status(200).json({
+    message: "User data retrieved",
+    data: {
+      id: 1,
+      name: "parsa",
+      username: "parsa",
+      links: [
+        { name: "website", url: "https://parsam.io" },
+        { name: "github", url: "https://github.com/pzrsa" },
+        { name: "twitter", url: "https://twitter.com/pzrsaa" },
+      ],
+    },
+  });
 });
 
 export default router;
